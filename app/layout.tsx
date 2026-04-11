b4992aeefdf2eb29e9eb9725339a703f64ebb6ef
cw-satou <cw.satou@gmail.com>
2026-04-12 07:41:03 +0900

    chore: app/globals.css app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,16 +28,6 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
         <link rel="apple-touch-icon" href="/icons/icon-192.png" />
       </head>
       <body>
